fd3b7209cafc825795cc64cb86ed9e3e80b40e8c
vk2012022 <99138478+vk2012022@users.noreply.github.com>
2024-05-03 18:49:02 +0300

    add new code cycle

--- main.py
+++ main.py
@@ -12,18 +12,16 @@ pygame.display.set_icon(icon)
 target_img = pygame.image.load("img/target.png")
 target_width = 80
 target_height = 80
-
-
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-
+move_frequency = 3000  # Частота смены позиции мишени в кадрах
+frame_count = 0  # Счетчик кадров
 
 running = True
 
 while running:
-
     screen.fill(color)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -34,6 +32,14 @@ while running:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
                 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+    frame_count += 1
+    if frame_count >= move_frequency:
+        target_x = random.randint(0, SCREEN_WIDTH - target_width)
+        target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+        frame_count = 0  # Сброс счетчика кадров после перемещения мишени
+
     screen.blit(target_img, (target_x, target_y))
     pygame.display.update()
+
 pygame.quit()
