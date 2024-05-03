2f6206c7370e00d028b4ee2f4540ef39f89b275c
vk2012022 <99138478+vk2012022@users.noreply.github.com>
2024-05-03 18:18:54 +0300

    add all cycle code

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/maxresdefault.jpg")
 pygame.display.set_icon(icon)
 
-target = pygame.image.load("img/target.png")
+target_img = pygame.image.load("img/target.png")
 target_width = 80
 target_height = 80
 
@@ -24,6 +24,16 @@ running = True
 
 while running:
 
-    pass
-
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 pygame.quit()
